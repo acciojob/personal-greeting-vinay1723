8b916c29c52952406fa03c782d9961331889154f
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-09-06 15:50:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      {name && <p>Hello, {name}!</p>}
+      {name && <p>Hello {name}!</p>}
     </div>
   );
 };
